fb9c3078997579cb9e12759c66303662d4d75b1a
Diogo Dias <diogodiasgrilo@gmail.com>
2023-06-09 21:12:56 +0100

    Updated to handle the asdw, left, and right keys

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 11:30:49 by diogpere          #+#    #+#             */
-/*   Updated: 2023/06/09 20:25:15 by diogpere         ###   ########.fr       */
+/*   Updated: 2023/06/09 21:10:57 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	free_map_exit(void *game)
 
 void	handle_angles(t_game *g, int key)
 {
-	if (key == 100)
+	if (key == 65363)
 	{
 		g->pa += 0.15;
 		if (g->pa > 2 * PI)
@@ -40,7 +40,7 @@ void	handle_angles(t_game *g, int key)
 		g->pdx = cos(g->pa) * MINIMAP_RATIO;
 		g->pdy = sin(g->pa) * MINIMAP_RATIO;
 	}
-	else if (key == 97)
+	else if (key == 65361)
 	{
 		g->pa -= 0.15;
 		if (g->pa < 0)
@@ -50,7 +50,7 @@ void	handle_angles(t_game *g, int key)
 	}
 }
 
-void	handle_directions(t_game *g, int one_dif)
+void	handle_directions_1(t_game *g, int one_dif)
 {
 	if (g->map[g->py][g->px - one_dif] != '1' && \
 		g->pa < 3.983188 && g->pa > 1.883189)
@@ -66,17 +66,38 @@ void	handle_directions(t_game *g, int one_dif)
 		g->py += one_dif;
 }
 
+void	handle_directions_2(t_game *g, int one_dif)
+{
+	if (g->map[g->py - one_dif][g->px] != '1' && \
+		(g->pa > 5.983186 || g->pa < 0.800000))
+		g->py -= one_dif;
+	else if (g->map[g->py + one_dif][g->px] != '1' \
+		&& (g->pa < 3.983188 && g->pa > 1.883189))
+		g->py += one_dif;
+	else if (g->map[g->py][g->px - one_dif] != '1' \
+		&& g->pa < 5.983186 && g->pa > 3.983188)
+		g->px -= one_dif;
+	else if (g->map[(g->py)][g->px + one_dif] != '1' \
+		&& g->pa < 1.883189 && g->pa > 0.783189)
+		g->px += one_dif;
+}
+
 int	ft_input(int key, void *param)
 {
 	t_game	*g;
 
 	g = (t_game *)param;
-	if (key == 100 || key == 97)
+	printf("key: %d\n", key);
+	if (key == 65363 || key == 65361)
 		handle_angles(g, key);
 	else if (key == 119)
-		handle_directions(g, 1);
+		handle_directions_1(g, 1);
 	else if (key == 115)
-		handle_directions(g, -1);
+		handle_directions_1(g, -1);
+	else if (key == 97)
+		handle_directions_2(g, 1);
+	else if (key == 100)
+		handle_directions_2(g, -1);
 	else if (key == 65307)
 		exit(0);
 	draw_rays(g);
