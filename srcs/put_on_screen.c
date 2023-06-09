337631aca1a0fc334d525c85fc97e93e3c3e9740
Diogo Dias <diogodiasgrilo@gmail.com>
2023-06-09 18:39:24 +0100

    Updated the put on screen file

--- srcs/put_on_screen.c
+++ srcs/put_on_screen.c
@@ -6,7 +6,7 @@
 /*   By: diogpere <diogpere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 17:18:47 by diogpere          #+#    #+#             */
-/*   Updated: 2023/06/09 18:33:35 by diogpere         ###   ########.fr       */
+/*   Updated: 2023/06/09 18:38:48 by diogpere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 void    determine_color (t_put_on_screen        *sc)
 {
-    float max_distance = 0.0f;
-    float min_distance = 200.0f;
-    float height = 0.0f;
+	t_determine_color dc;
+	
+    dc.max_distance = 0.0f;
+    dc.min_distance = 200.0f;
+    dc.height = 0.0f;
 
     sc->color = 0xFFFFFF;
-    if (sc->proj_height > min_distance) {
-        height = min_distance;
-    } else if (sc->proj_height < max_distance) {
-        height = max_distance;
+    if (sc->proj_height > dc.min_distance) {
+        dc.height = dc.min_distance;
+    } else if (sc->proj_height < dc.max_distance) {
+        dc.height = dc.max_distance;
     }
     else
-        height = sc->proj_height;
-    float ratio = (height - min_distance) / (max_distance - min_distance);
-    int white = 0xFFFFFF;
-    int grey = 0x808080;
-    int red = (int)(((white >> 16) & 0xFF) * (1.0f - ratio) + ((grey >> 16) & 0xFF) * ratio);
-    int green = (int)(((white >> 8) & 0xFF) * (1.0f - ratio) + ((grey >> 8) & 0xFF) * ratio);
-    int blue = (int)((white & 0xFF) * (1.0f - ratio) + (grey & 0xFF) * ratio);
-    sc->color = (red << 16) | (green << 8) | blue;
+        dc.height = sc->proj_height;
+    dc.ratio = (dc.height - dc.min_distance) / (dc.max_distance - dc.min_distance);
+    dc.white = 0xFFFFFF;
+    dc.grey = 0x808080;
+    dc.red = (int)(((dc.white >> 16) & 0xFF) * (1.0f - dc.ratio) + ((dc.grey >> 16) & 0xFF) * dc.ratio);
+    dc.green = (int)(((dc.white >> 8) & 0xFF) * (1.0f - dc.ratio) + ((dc.grey >> 8) & 0xFF) * dc.ratio);
+    dc.blue = (int)((dc.white & 0xFF) * (1.0f - dc.ratio) + (dc.grey & 0xFF) * dc.ratio);
+    sc->color = (dc.red << 16) | (dc.green << 8) | dc.blue;
 }
 
 void	put_on_screen(t_game *g, t_line_drawing *rs, float ray_angle, int ray)
